fcf4589dc0d934f465953cf32cf90a90367b2b4d
David Majdandžić <dmajdandzic@neos.hr>
2023-01-18 20:08:29 +0100

    Implement routing

--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
+import {createApp} from 'vue'
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
+import UserList from "@/pages/User/List/UserList.vue";
 import UserAdd from "@/pages/User/Add/UserAdd.vue";
 
-import {createApp} from 'vue'
-import {createRouter, createWebHistory} from "vue-router";
+const routes = [
+	{
+		path: '/',
+		component: UserList
+	},
+	{
+		path: '/user',
+		component: UserList
+	},
+	{
+		path: '/user/add',
+		component: UserAdd
+	},
+]
 
 const router = createRouter({
-	                            history: createWebHistory(),
-	                            routes: [
-		                            {
-			                            path: '/',
-			                            component: UserAdd
-		                            },
-		                            {
-			                            path: '/user',
-			                            component: UserAdd
-		                            },
-		                            {
-			                            path: '/user/add',
-			                            component: UserAdd
-		                            },
-	                            ],
-                            })
+	                                      history: createWebHistory(),
+	                                      routes,
+                                      })
 
 const app = createApp({})
 app.use(router)
 
-window.vm = app.mount('#app')
+app.mount('#app')
